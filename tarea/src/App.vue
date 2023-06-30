84f84e2872c5183793a1122958b6a6fb6c119518
Angel_zapata <22170124@uttcampus.edu.mx>
2023-06-30 00:43:09 -0600

    me falta mucho

---
+++ tarea/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+ <v-app id="inspire">
+    <v-navigation-drawer v-model="drawer">
+
+      <RouterView></RouterView>
+     
+    </v-navigation-drawer>
+
+    <v-app-bar>
+      
+      <v-app-bar-nav-icon @click="drawer = !drawer">
+      
+      </v-app-bar-nav-icon>
+<RouterLink to="/"></RouterLink>
+      <v-toolbar-title>Application</v-toolbar-title>
+    </v-app-bar>
+
+    <v-main>
+     
+    </v-main>
+  </v-app>
+</template>
+
+<script setup>
+  import { ref } from 'vue'
+
+  const drawer = ref(null)
+</script>
+
+<script>
+  export default {
+    data: () => ({ drawer: null }),
+    components: { RouterLink, RouterView }
+}
+</script>
